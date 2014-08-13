4650d74dfcb32ec209bc255c64268846c582e43d
Vilian Zhekov <vzzhekov@gmail.com>
2014-08-13 17:34:22 +0300

    major UI changes

--- model/kendo.js
+++ model/kendo.js
@@ -21,7 +21,41 @@ $(document).ready(function() {
 
     $("#primarybutton").click(function(){
         var d = 'Success!';
-        popupNotification.show(d, "success");
+        var f = 'Fail';
+        var fv = document.getElementById('future_value').value;
+        var ir = document.getElementById('interest_rate').value;
+        var ny = document.getElementById('number_of_years').value;
+        if(fv == "" || ir == "" || ny == "") {
+            popupNotification.show(f, "error");
+        }
+        else {
+            popupNotification.show(d,"success");
+        }
     });
 
+});
+
+$(document).ready(function() {
+    // create NumericTextBox from input HTML element
+//    $("#numeric").kendoNumericTextBox();
+
+    // create Curerncy NumericTextBox from input HTML element
+    $("#future_value").kendoNumericTextBox({
+        format: "c",
+        decimals: 2
+    });
+
+    // create Percentage NumericTextBox from input HTML element
+    $("#interest_rate").kendoNumericTextBox({
+        format: "p0",
+        min: 0,
+        max: 0.1,
+        step: 0.01
+    });
+
+    // create NumericTextBox from input HTML element using custom format
+    $("#number_of_years").kendoNumericTextBox({
+        format: "a",
+        decimals: 2
+    });
 });
